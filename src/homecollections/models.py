1c6bb9ba6378737fd1086ac8143ca18c7395386e
zǝʇɹoɔ oɓıɹpoɹ <28023409+roicort@users.noreply.github.com>
2024-01-25 17:54:58 -0600

    Update models.py

--- src/homecollections/models.py
+++ src/homecollections/models.py
@@ -10,7 +10,7 @@ class HomeCollection(models.Model):
     name = models.CharField(max_length=128, unique=True, blank=False, null=False)
     material_icon = models.CharField(max_length=128, blank=True, null=True)
     link_type = models.CharField(max_length=128 ,choices=[('keywords', 'Keywords'), ('external', 'External')])
-    url = models.CharField(max_length=128, unique=True, blank=True, null=True)
+    url = models.CharField(max_length=128, blank=True, null=True)
     keywords = models.ManyToManyField(HierarchicalKeyword, blank=True)
 
     def __str__(self):
